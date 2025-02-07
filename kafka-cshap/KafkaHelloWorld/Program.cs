e82e2c9da6b2ebe7f33a329d4a3541f02fffb974
EDanilchenko <Danilchenko@outlook.com>
2025-02-07 12:49:54 +0100

    init

---
+++ kafka-cshap/KafkaHelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace KafkaHelloWorld {
+  class Program {
+    public static void Main(string[] args) {
+      var config = new ProducerConfig {
+        BootstrapServers = "localhost:9092"
+      };
+
+      using (var producer = new ProducerBuilder<Null, string>(config).Build()) {
+        try {
+          var result = producer.ProduceAsync("test-topic", new Message<Null, string> { Value = "Hello Kafka from C#" }).Result;
+          Console.WriteLine($"Message sent to {result.TopicPartitionOffset}");
+        } catch (ProduceException<Null, string> e) {
+          Console.WriteLine($"Error sending message: {e.Error.Reason}");
+        }
+      }
+    }
+  }
+}
